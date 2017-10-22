8579b5982979be7a91b3142d9eb35ed0f390c281
cpkurotori <ckurotori2@ohlone.edu>
2017-10-22 14:36:00 -0700

    Final

--- myliteracy/js/references.js
+++ myliteracy/js/references.js
@@ -1,4 +1,10 @@
-var references = {'census':'https://view.officeapps.live.com/op/view.aspx?src=https://www2.census.gov/programs-surveys/demo/tables/computer-internet/time-series/computer-use-1984-2009/appendix-tablea.xls',
+var references = {
+  'Scribner':'Scribner, Sylvia. “Literacy in Three Metaphors.” American Journal of Education, vol. 93, no. 1, 1984, pp. 6–21. JSTOR, JSTOR, www.jstor.org/stable/1085087.',
+  'Vygotsky': 'Vygotsky, L.S., et al. Mind in Society: the Development of Higher Psychological Processes. Harvard University Press, 1978.',
+  'Gee': 'Gee, James Paul. “LITERACY, DISCOURSE, AND LINGUISTICS: INTRODUCTION.” The Journal of Education, vol. 171, no. 1, 1989, pp. 18–25. JSTOR, JSTOR, www.jstor.org/stable/42743865.',
+  'United': 'United States Census Bureau. “Computer and Internet Access in the United States: 2012.” U.S. Census Bureau, 3 Feb. 2014, www.census.gov/data/tables/2012/demo/computer-internet/computer-use-2012.html.',
+  'Techopedia': 'Techopedia. “What Is the Digital Revolution? - Definition from Techopedia.” Techopedia.com, Techopedia Inc, www.techopedia.com/definition/23371/digital-revolution. n.d.'
+  
 };
 
 var sorted_ref = Object.keys(references).sort()
