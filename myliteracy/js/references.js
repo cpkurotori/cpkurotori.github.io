8913905b48d5c0683a487915f412db72838deda8
cpkurotori <ckurotori2@ohlone.edu>
2017-10-22 03:12:34 -0700

    census

--- myliteracy/js/references.js
+++ myliteracy/js/references.js
@@ -1,4 +1,4 @@
-var references = {'firstword':'Reference Text',
+var references = {'census':'https://view.officeapps.live.com/op/view.aspx?src=https://www2.census.gov/programs-surveys/demo/tables/computer-internet/time-series/computer-use-1984-2009/appendix-tablea.xls',
 };
 
 var sorted_ref = Object.keys(references).sort()
