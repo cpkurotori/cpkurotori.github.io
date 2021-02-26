7115b50fd60266c50be0e6fa8775134c5134d8cd
Cameron Kurotori <cameron.kurotori@ibm.com>
2021-02-25 19:21:37 -0800

    add stop button and warnings

--- js/server-start.js
+++ js/server-start.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
-    function create_action(state) {
-        $("#start").prop('disabled', true);
+    function post_create(state) {
+        $(".disable-on-click").prop('disabled', true);
         $.ajax({
             url: "https://vjkqhynan2.execute-api.us-east-2.amazonaws.com/default",
             data: JSON.stringify({
@@ -10,16 +10,63 @@ $(document).ready(() => {
             dataType: "json",
             success: function (result) {
                 console.log(result);
-                $("#main").html(`<code>${result}<code>`);
+                let output = `<h2>Success!</h2>`;
+                if (state == "start") {
+                    output += "<br><p>The server should start up any moment. " +
+                        "The server will turn off automatically after 12 hours. " +
+                        "However, if you stop playing for an extended period of time, " +
+                        "it would be wonderful " +
+                        "if you could run the `/stop` command in the chat and come " +
+                        "back <a href='https://camkuro.com/mc.html'>here</a> and stop the server. " +
+                        "This will help me limit the costs. If you forget, it's literally not a problem! " +
+                        "(as a reference it only costs 4 cents per hour that the server is running :P)";
+                } else if (state == "stop") {
+                    output += "<br><p>Thank you so much for stopping the server. I love you <3</p>";
+                }
+                output += `<br><br><code style='color:rgb(232,232,232)'>${JSON.stringify(result)}</code>`
+                $("#main").html(output);
             },
             error: function (result) {
                 console.log(result);
                 $("#main").html(`<code>${result.responseText}<code>`);
             }
-        });
+        });  
     }
 
-    $("#start").click(function(){
+    function create_action(state) {
+        if (state == "stop") {
+            swal({
+                buttons: {
+                    confirm: {
+                        text: "Yes",
+                        value: true,
+                        visible: true,
+                        closeModal: true
+                    },
+                    cancel: {
+                        text: "No",
+                        visible: true,
+                        closeModal: true,
+                        value: false
+                    }
+                },
+                icon: "warning",
+                text: "Did you run `/stop` command in the minecraft server chat and did the server come to a complete stop?"
+            }).then((confirmed) => {
+                if (!confirmed) {
+                    return
+                }
+                post_create(state);
+            });
+        } else {
+            post_create(state);
+        }
+    }
+
+    $("#start").click(function () {
         create_action("start");
+    });
+    $("#stop").click(function () {
+        create_action("stop");
     });
 });
